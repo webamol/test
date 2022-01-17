003f6320af3d5bcb3afd11bdf8e69f6eb7f277cb
webamol <amol.punyanagari@gmail.com>
2022-01-17 19:25:03 +0530

    firstgit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+    child: Text(
+      "Welcome",
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
